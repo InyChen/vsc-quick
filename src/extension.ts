19cde719b4917f6f49cdcd68d1ad73958cf7e905
350874218 <350874218@qq.com>
2019-10-23 15:44:55 +0800

    修改欢迎词

--- src/extension.ts
+++ src/extension.ts
@@ -20,21 +20,7 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.window.showInformationMessage('Hello World!');
 	});
 
-
-	// 创建webview
-	const panel = vscode.window.createWebviewPanel(
-		'testWebview', // viewType
-		"WebView演示", // 视图标题
-		vscode.ViewColumn.One, // 显示在编辑器的哪个部位
-		{
-			enableScripts: true, // 启用JS，默认禁用
-			retainContextWhenHidden: true, // webview被隐藏时保持状态，避免被重置
-		}
-	);
-	panel.webview.html = `<html><body>你好，我是Webview</body></html>`;
-
-
-
+	vscode.window.showInformationMessage('欢迎使用quick');
 	context.subscriptions.push(disposable);
 }
 
